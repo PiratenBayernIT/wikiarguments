30fc38b23a2bbca193b143dae2b074958cb5e09b
Thomas <entropy@heterarchy.net>
2014-07-30 19:51:34 +0200

    replace wikiarguments with bptarguments, get rid of unused messages

--- sql/structure.sql
+++ sql/structure.sql
@@ -2,7 +2,7 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Database: `wikiarguments`
+-- Database: `bptarguments`
 --
 
 -- --------------------------------------------------------
