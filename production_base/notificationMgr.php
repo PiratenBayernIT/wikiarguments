50745b3ff1d42381df0481379fc30eb8f8929204
Thomas <entropy@heterarchy.net>
2014-09-23 14:12:12 +0200

    improved notification subject, Frage->Antrag

--- production_base/notificationMgr.php
+++ production_base/notificationMgr.php
@@ -80,7 +80,8 @@ class NotificationMgr
                 $url         = $a->fullurl();
             }
 
-            $subject = $sTemplate->getString("NOTIFICATION_NEW_ARGUMENT_SUBJECT");
+            $subject = $sTemplate->getString("NOTIFICATION_NEW_ARGUMENT_SUBJECT",
+                Array("[QUESTION]", "[ARGUMENT]"), Array($q->title(), $a->headline());
             $message = $sTemplate->getString("NOTIFICATION_NEW_ARGUMENT_BODY",
                                              Array("[USERNAME]", "[AUTHOR]", "[URL]", "[QUESTION]", "[ARGUMENT]", "[UNFOLLOW_URL]", "[PROFILE_URL]"),
                                              Array($row->userName, $a->author(), $url, $q->title(), $a->headline(), $unfollowUrl, $profileUrl));
