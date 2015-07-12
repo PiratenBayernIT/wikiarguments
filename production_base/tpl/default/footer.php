77003701e3f61a1c258a2da46c27e91d52ef3016
escaP <escaP@gmx.de>
2015-07-12 17:09:56 +0200

    added other link to footer area
    Conflicts:
    	sql/data.sql

--- production_base/tpl/default/footer.php
+++ production_base/tpl/default/footer.php
@@ -68,6 +68,7 @@ wikiargument.raiseNotice("<? echo $notice; ?>");
         <div id = "footer_copyright">
           <a href ="<? echo $sTemplate->getString("FAQ_URL"); ?>"><? echo $sTemplate->getString("FOOTER_FAQ"); ?></a> |
           <a href ="<? echo $sTemplate->getString("TERMS_OF_USE_URL"); ?>"><? echo $sTemplate->getString("TERMS_OF_USE"); ?></a> |
+          <a href ="<? echo $sTemplate->getString("FOOTER_OTHER_URL"); ?>"><? echo $sTemplate->getString("FOOTER_OTHER"); ?></a> |
           <? echo $sTemplate->getString("FOOTER_COPYRIGHT"); ?> |
           <a href = '#' onclick = "$('#footer_menu').show(); return false;"><? echo $sTemplate->getString("FOOTER_MENU"); ?></a> |
           <a href = 'http://www.wikiarguments.net'>Powered by Wikiarguments</a>
